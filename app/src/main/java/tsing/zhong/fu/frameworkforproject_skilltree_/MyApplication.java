1b0bf0778cfbb9b5f10b8b8edf7ef05a61b967f7
Zhongqing Fu <fuzhongqing1995@gmail.com>
2015-07-25 17:36:53 +0800

    Convert all files to UTF-8

--- app/src/main/java/tsing/zhong/fu/frameworkforproject_skilltree_/MyApplication.java
+++ app/src/main/java/tsing/zhong/fu/frameworkforproject_skilltree_/MyApplication.java
@@ -2,19 +2,21 @@ package tsing.zhong.fu.frameworkforproject_skilltree_;
 
 import android.app.Application;
 
+import org.json.JSONObject;
+
 import tsing.zhong.fu.frameworkforproject_skilltree_.utils.Loger;
 
 /**
  * Created by fuzho on 2015/7/25.
  *
- * 全局变量的Holder
+ * 鍏ㄥ眬鍙橀噺鐨凥older
  *
  */
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        
     }
 
 }
